053be6224706f84d920e0e7fe274e5549f94bf12
ngohongthong <baphongpine@gmail.com>
2023-02-06 09:57:51 +0700

    haha

--- src/redux/initState.js
+++ src/redux/initState.js
@@ -1,18 +1,18 @@
 // Avoid initSate remove 
 // run this coed to add a new initState 
-export const initState1 = {
-    filter : {
-        search : '',
-        status : 'All',
-        priority : '',
-    },
-    todoList : [
-        { id : 1 , name : "Learn React", completed : false , priority : 'High'},
-        { id : 2 , name : "Learn Redux", completed : false , priority : 'High'},
-        { id : 3 , name : "Learn nodeJS", completed : false , priority : 'Low'}, 
-    ]
-}
-localStorage.setItem('initState',JSON.stringify(initState1))
+// export const initState1 = {
+//     filter : {
+//         search : '',
+//         status : 'All',
+//         priority : '',
+//     },
+//     todoList : [
+//         { id : 1 , name : "Learn React", completed : false , priority : 'High'},
+//         { id : 2 , name : "Learn Redux", completed : false , priority : 'High'},
+//         { id : 3 , name : "Learn nodeJS", completed : false , priority : 'Low'}, 
+//     ]
+// }
+// localStorage.setItem('initState',JSON.stringify(initState1))
 
 
 
